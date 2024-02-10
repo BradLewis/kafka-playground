6cf1b48d6326e8d9e769fb681fb10247504d2764
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2024-02-10 14:13:34 -0500

    create partition consumer in separate goroutines

--- consumer.go
+++ consumer.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/segmentio/kafka-go"
@@ -32,6 +35,16 @@ func launchWorker(id string) {
 			})
 	}
 	defer reader.Close()
+	defer fmt.Printf("Worker %s is done\n", id)
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		reader.Close()
+		fmt.Printf("Worker %s is done\n", id)
+
+		os.Exit(1)
+	}()
 
 	fmt.Printf("Worker %s is listening\n", id)
 	for {
@@ -52,7 +65,47 @@ func launchWorker(id string) {
 }
 
 func main() {
-	go launchWorker("1")
-	go launchWorker("2")
-	launchWorker("3")
+	// launchWorker(os.Args[1])
+	startGoroutineWorkers()
+
+}
+
+func startGoroutineWorkers() {
+	topic := "my-topic"
+	reader := kafka.NewReader(
+		kafka.ReaderConfig{
+			Brokers:  []string{"localhost:51451"},
+			Topic:    topic,
+			MaxBytes: 10e6,
+			GroupID:  "my-group-2",
+			MaxWait:  1 * time.Second,
+		})
+	defer reader.Close()
+	chanMap := make(map[int]chan *kafka.Message)
+	for {
+		m, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			log.Fatal("error reading message", err)
+		}
+		if _, ok := chanMap[m.Partition]; !ok {
+			log.Printf("Creating new channel for partition %d\n", m.Partition)
+			chanMap[m.Partition] = make(chan *kafka.Message, 256)
+			go partitionConsumer(fmt.Sprintf("Partition %d", m.Partition), chanMap[m.Partition])
+		}
+		chanMap[m.Partition] <- &m
+	}
+}
+
+func partitionConsumer(name string, messageChan chan *kafka.Message) {
+	for message := range messageChan {
+		log.Printf(
+			"name: %s - message at topic/partition/offset %v/%v/%v: %s = %s\n",
+			name,
+			message.Topic,
+			message.Partition,
+			message.Offset,
+			string(message.Key),
+			string(message.Value),
+		)
+	}
 }
--- producer.go
+++ producer.go
@@ -2,36 +2,74 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
+	"math/rand"
+	"time"
 
 	"github.com/segmentio/kafka-go"
 )
 
+type Data struct {
+	Timestamp  time.Time
+	Value      float64
+	MetricName string
+}
+
 func main() {
 	w := &kafka.Writer{
-		Addr:     kafka.TCP("localhost:50321"),
-		Topic:    "my-topic",
-		Balancer: &kafka.Hash{},
+		Addr:            kafka.TCP("localhost:51451"),
+		Topic:           "my-topic",
+		Balancer:        &kafka.Hash{},
+		WriteBackoffMax: 100 * time.Millisecond,
+		Async:           true,
 	}
+	ticker := time.NewTicker(2000 * time.Millisecond)
 
-	err := w.WriteMessages(context.Background(),
-		kafka.Message{
-			Key:   []byte("Key-A"),
-			Value: []byte("Hello World!"),
-		},
-		kafka.Message{
-			Key:   []byte("Key-B"),
-			Value: []byte("One!"),
-		},
-		kafka.Message{
-			Key:   []byte("Key-C"),
-			Value: []byte("Two!"),
-		},
-	)
-	if err != nil {
-		log.Fatal("failed to write messages:", err)
+	for {
+		select {
+
+		case <-ticker.C:
+			log.Println("writing messages")
+			data := Data{
+				Timestamp:  time.Now(),
+				Value:      rand.Float64(),
+				MetricName: "my-metric",
+			}
+			dataBytes, err := json.Marshal(data)
+			if err != nil {
+				log.Printf("failed to marshal data: %v", err)
+				continue
+			}
+			err = w.WriteMessages(context.Background(),
+				kafka.Message{
+					Key:   []byte("Key-A"),
+					Value: []byte("Hello World!"),
+				},
+				kafka.Message{
+					Key:   []byte("Key-B"),
+					Value: []byte("One!"),
+				},
+				kafka.Message{
+					Key:   []byte("Key-C"),
+					Value: []byte("Two!"),
+				},
+				kafka.Message{
+					Key:   []byte("Key-D"),
+					Value: []byte("abcdefg"),
+				},
+				kafka.Message{
+					Key:   []byte("Key-E"),
+					Value: dataBytes,
+				},
+			)
+			if err != nil {
+				log.Fatal("failed to write messages:", err)
+			}
+			log.Println("wrote messages")
+
+		}
 	}
-
 	if err := w.Close(); err != nil {
 		log.Fatal("failed to close writer:", err)
 	}
